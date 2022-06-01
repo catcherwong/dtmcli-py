02d3b39236eb9b6e5125b1477bf2e6f1c2d66c1b
catcherwong <catcher_hwq@outlook.com>
2022-06-01 18:03:09 +0800

    improve tcc

--- dtmcli/dtmimp.py
+++ dtmcli/dtmimp.py
@@ -9,3 +9,29 @@ def trans_call_dtm(dtm, body, operation, request_timeout):
     url = "%s/%s" % (dtm, operation)
     r = requests.post(url, json=body, timeout=request_timeout)
     utils.check_result(r)
+
+
+def trans_register_branch(dtm, body, added, operation, request_timeout):
+    for k, v in added.items():
+        body[k] = v
+
+    trans_call_dtm(dtm, body, operation, request_timeout)
+
+
+def trans_request_branch(dtm, tb, method, body, branch_id, op, url):
+    if url == "":
+        return None
+
+    query = {
+        "dtm":        dtm,
+        "gid":        tb.gid,
+        "branch_id":  branch_id,
+        "trans_type": tb.trans_type,
+        "op":         op,
+    }
+
+    r = requests.request(method=method, url=url,
+                         headers=tb.branch_headers, params=query, json=body)
+    utils.resp_as_error_compatible(r)
+
+    return r
--- dtmcli/tcc.py
+++ dtmcli/tcc.py
@@ -1,60 +1,69 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-import requests
 import traceback
 import sys
 import json
-from dtmcli import utils
+from dtmcli import dtmimp, transbase, utils
+
 
 class Tcc(object):
-  def __init__(self, dtmUrl, gid):
-    self.dtm = dtmUrl
-    self.gid = gid
-    self.id_generator = utils.IdGenerator()
-  def call_branch(self, body, tryUrl, confirmUrl, cancalUrl):
-      branch_id = self.id_generator.new_branch_id()
-      r = requests.post(self.dtm + "/registerBranch", json={
-          "gid": self.gid,
-          "branch_id": branch_id,
-          "trans_type": "tcc",
-          "status": "prepared",
-          "data": json.dumps(body),
-          "confirm": confirmUrl,
-          "cancel": cancalUrl,
-      })
-      utils.check_result(r)
-      r = requests.post(tryUrl, json=body, params={
-          "gid": self.gid,
-          "trans_type": "tcc",
-          "branch_id": branch_id,
-          "op": "try",
-      })
-      utils.check_result(r)
-      return r
+    def __init__(self, dtmUrl, gid):
+        self.dtm = dtmUrl
+        self.trans_base = transbase.TransBase(gid, "tcc")
+        self.id_generator = utils.IdGenerator()
+
+    def call_branch(self, body, tryUrl, confirmUrl, cancalUrl):
+        branch_id = self.id_generator.new_branch_id()
+
+        tb = {
+            "gid": self.trans_base.gid,
+            "trans_type": self.trans_base.trans_type,
+            "custom_data": self.trans_base.custom_data,
+        }
+
+        added = {
+            "data":          json.dumps(body),
+            "branch_id":      branch_id,
+            "confirm": confirmUrl,
+            "cancel": cancalUrl,
+        }
+
+        dtmimp.trans_register_branch(
+            self.dtm, body=tb, added=added, operation="registerBranch", request_timeout=8)
+
+        r = dtmimp.trans_request_branch(
+            self.dtm, self.trans_base, "POST", body, branch_id, "try", tryUrl)
+
+        return r
+
 
 def tcc_global_transaction(dtmUrl, gid, tcc_cb):
+    return tcc_global_transaction2(dtmUrl, gid, lambda t: None, tcc_cb)
+
+
+def tcc_global_transaction2(dtmUrl, gid, custom, tcc_cb):
     tcc = Tcc(dtmUrl, gid)
-    tbody = {
-        "gid": tcc.gid,
-        "trans_type": "tcc",
-    }
+    custom(tcc)
+
     try:
-        r = requests.post(tcc.dtm + "/prepare", json=tbody)
-        utils.check_result(r)
+        dtmimp.trans_call_dtm(tcc.dtm, tcc.trans_base.__dict__,
+                              "prepare", tcc.trans_base.request_timeout)
         tcc_cb(tcc)
-        r = requests.post(tcc.dtm + "/submit", json=tbody)
-        utils.check_result(r)
+        dtmimp.trans_call_dtm(tcc.dtm, tcc.trans_base.__dict__,
+                              "submit", tcc.trans_base.request_timeout)
     except:
         traceback.print_exception(*sys.exc_info())
-        r = requests.post(tcc.dtm + "/abort", json=tbody)
-        utils.check_result(r)
+        dtmimp.trans_call_dtm(tcc.dtm, tcc.trans_base.__dict__,
+                              "abort", tcc.trans_base.request_timeout)
         return ""
-    return tcc.gid
+    return gid
+
 
 def tcc_from_req(dtmUrl, gid, branch_id):
     if dtmUrl == "" or gid == "" or branch_id == "":
-        raise Exception("bad tcc req info: dtm %s gid %s branch_id %s" % (dtmUrl, gid, branch_id))
+        raise Exception("bad tcc req info: dtm %s gid %s branch_id %s" % (
+            dtmUrl, gid, branch_id))
     tcc = Tcc(dtmUrl, gid)
     tcc.id_generator = utils.IdGenerator(branch_id)
     return tcc
